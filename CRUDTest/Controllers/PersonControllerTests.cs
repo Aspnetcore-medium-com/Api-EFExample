2ce061ac946bfa1ef837cf6614e9c25c1948cd7d
raja <rashok-adm@transportforlondon.onmicrosoft.com>
2026-02-18 12:30:30 +0000

    changes

--- CRUDTest/Controllers/PersonControllerTests.cs
+++ CRUDTest/Controllers/PersonControllerTests.cs
@@ -75,5 +75,61 @@ namespace CRUDTest.Controllers
             var problem = notFoundResult.Value as ProblemDetails;
             problem!.Title.Should().Be("Person Not Found");
         }
+
+        [Fact]
+        public async Task Delete_WhenPersonExists_ShouldDeleteAndReturnNoContent()
+        {
+            // Arrange
+            var personId = Guid.NewGuid();
+            _personServiceMock.Setup(s => s.DeletePerson(personId, It.IsAny<CancellationToken>())).ReturnsAsync(true);
+
+            // Act
+            var response = await _sut.Delete(personId, CancellationToken.None);
+
+            // Assert
+            var NoContentResult = response as NoContentResult;
+            NoContentResult!.StatusCode.Should().Be(StatusCodes.Status204NoContent);
+        }
+
+        [Fact]
+        public async Task Delete_WhenPersonNotFound_ShouldReturnNotFound()
+        {
+            // Arrange
+            Guid personId = Guid.NewGuid();
+            _personServiceMock.Setup(r => r.DeletePerson(personId,It.IsAny<CancellationToken>())).ReturnsAsync(false);
+            // Act
+            var response = await _sut.Delete(personId,CancellationToken.None);
+
+            // Assert
+            var notFoundResult = response as NotFoundObjectResult;
+            notFoundResult!.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+            var problemDetails = notFoundResult.Value as ProblemDetails;
+            problemDetails!.Title.Should().Be("Person not found");
+        }
+
+        //public async Task<ActionResult<PersonResponse>> Add(PersonAddRequest personAddRequest, CancellationToken cancellationToken = default)
+        //{
+        //    PersonResponse personResponse = await _personService.AddPerson(personAddRequest, cancellationToken);
+        //    return CreatedAtAction(nameof(GetById), new { personId = personResponse.PersonId }, personResponse);
+        //}
+
+        [Fact]
+        public async Task Add_WhenValidRequest_ShouldReturnCreatedAtAction()
+        {
+            // Arrange
+            var request = new PersonAddRequest() {  PersonName = "Test" };
+            var response = new PersonResponse() { PersonName = "Test" , PersonId = Guid.NewGuid()};
+            _personServiceMock.Setup(r => r.AddPerson(request, It.IsAny<CancellationToken>())).ReturnsAsync(response);
+            //Act
+            var result = await _sut.Add(request, It.IsAny<CancellationToken>());
+
+            // Assert
+            var CreatedAtActionResult = result.Result as CreatedAtActionResult;
+            CreatedAtActionResult.Should().NotBeNull();
+            CreatedAtActionResult.ActionName.Should().Be("GetById");
+            CreatedAtActionResult.Value.Should().BeEquivalentTo(response);
+        }
+
+
     }
 }
