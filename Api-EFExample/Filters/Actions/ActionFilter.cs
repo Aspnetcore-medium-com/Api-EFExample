27441f5d5b91d96512acbbbb4a9a5eb6e27ee9e9
raja <rashok-adm@transportforlondon.onmicrosoft.com>
2026-02-24 07:04:34 +0000

    changes

--- Api-EFExample/Filters/Actions/ActionFilter.cs
+++ Api-EFExample/Filters/Actions/ActionFilter.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using Api_EFExample.Controllers;
+using FluentValidation;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -17,25 +18,30 @@ namespace Api_EFExample.Filters.Actions
 
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            // ActionArguments.Values is ICollection<object> 
-            // This safely casts the object to type T
-            // Find the first action parameter that is PersonAddRequest
-            //If not matching → returns null(no exception).
-            var model = context.ActionArguments.Values.FirstOrDefault(x => x is T) as T;
-            //Skip validation and continue.
-            if (model == null) { 
+            if (context.Controller is PersonController personController)
+            {
+                // ActionArguments.Values is ICollection<object> 
+                // This safely casts the object to type T
+                // Find the first action parameter that is PersonAddRequest
+                //If not matching → returns null(no exception).
+                var model = context.ActionArguments.Values.FirstOrDefault(x => x is T) as T;
+                //Skip validation and continue.
+                if (model == null)
+                {
+                    await next();
+                }
+
+                ValidationResult result = await _validator.ValidateAsync(model!);
+                //short circuiting
+                if (!result.IsValid)
+                {
+                    context.Result = new BadRequestObjectResult(
+                        result.Errors.Select(x => new { x.PropertyName, x.ErrorMessage })
+                        );
+                    return;
+                }
                 await next();
             }
-
-            ValidationResult result = await _validator.ValidateAsync(model!);
-            //short circuiting
-            if (!result.IsValid) {
-                context.Result = new BadRequestObjectResult(
-                    result.Errors.Select(x => new { x.PropertyName,x.ErrorMessage })
-                    );
-                return;
-            }
-            await next();
         }
     }
 
--- Api-EFExample/Filters/Actions/ResponseHeaderFilter.cs
+++ Api-EFExample/Filters/Actions/ResponseHeaderFilter.cs
@@ -4,8 +4,10 @@ using Microsoft.Extensions.Options;
 
 namespace Api_EFExample.Filters.Actions
 {
-    public class ResponseHeaderFilter : IAsyncActionFilter
+    public class ResponseHeaderFilter : IAsyncActionFilter,IOrderedFilter
     {
+        public int Order { get; set; } = 1;
+
         private readonly ILogger<ResponseHeaderFilter> _logger;
       
         private readonly HeaderOptions _options;
@@ -14,6 +16,8 @@ namespace Api_EFExample.Filters.Actions
             _logger = logger;
            _options = options.Value;
         }
+
+
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             context.HttpContext.Response.Headers[_options.Key] = _options.Value;
